a8260dc72505cbb478a7c432b86c59c782cf06c8
Rodolphe Bounatirou <rodolphe.bounatirou@gmail.com>
2023-07-23 15:13:00 +0200

    initial commit

---
+++ ExempleCommand/ExempleCommand/Cartes/CartesProperties.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExempleCommand.Cartes
+{
+    public static class CartesProperties
+    {
+        public static readonly int WIDTH = 8;
+        public static readonly int HEIGHT = 8;
+    }
+}
---
+++ ExempleCommand/ExempleCommand/Command/CommandAllerAGauche.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExempleCommand.Command
+{
+    public class CommandAllerAGauche : ICommand
+    {
+        private int nbPixel;
+        private Personnage sonReveiver;
+        
+        public CommandAllerAGauche(int pixel, Personnage cible)
+        {
+            nbPixel = pixel;
+            sonReveiver = cible;
+        }
+
+        public bool Executer()
+        {
+            return sonReveiver.DeplacerAGauche(nbPixel);
+        }
+
+        public bool Annuler()
+        {
+            return sonReveiver.DeplacerADroite(nbPixel);
+        }
+
+        public string GetString()
+        {
+            return "Gauche(" + nbPixel + ")";
+        }
+    }
+}
